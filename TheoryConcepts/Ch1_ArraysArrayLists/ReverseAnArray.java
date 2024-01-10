4df174b51503e17a01c83ce16d8d237b39d58c9c
iamsayannayek <sayannayek1912@gmail.com>
2024-01-10 20:15:53 +0530

    Added ReverseAnArray.java

---
+++ TheoryConcepts/Ch1_ArraysArrayLists/ReverseAnArray.java
@@ -0,0 +1,27 @@
+package TheoryConcepts.Ch1_ArraysArrayLists;
+
+import java.util.Arrays;
+
+public class ReverseAnArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverseArray(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<end){
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+}
