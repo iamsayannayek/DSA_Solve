f49f80b6aecca356dd9af2e4642da92b0f2e1145
iamsayannayek <sayannayek1912@gmail.com>
2024-01-11 07:51:59 +0530

    Updated Q1_SetMatrixZero.java

--- Ch1_ArraysQs/Q1_SetMatrixZero.java
+++ Ch1_ArraysQs/Q1_SetMatrixZero.java
@@ -21,8 +21,48 @@ public class Q1_SetMatrixZero {
     }
 
     static void setZeroes(int[][] matrix){
+        //int col[m] = matrix[0][...]
+        //int row[n] = matrix[...][0]
+        int col0 = 1;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    //mark the i-th row
+                    matrix[i][0] = 0;
+                    //mark the j-th col
+                    if(j!=0){
+                        matrix[0][j] = 0;
+                    }else{
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        //Mark the inner matrix
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for (int j=0; j<m; j++){
+                matrix[0][j] = 0;
+            }
+        }
 
+        if(col0 == 0){
+            for(int i=0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
+
     // It takes extra space so it's not optimize
     static void setZeroesExtraSpace(int[][] matrix) {
         int[] rowMark = new int[matrix.length];
