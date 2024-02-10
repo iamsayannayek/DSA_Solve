cdc738f730778a19dd5c686543ca5fa2ac1ea855
iamsayannayek <sayannayek1912@gmail.com>
2024-02-10 08:25:51 +0530

    Added Selection Sort

---
+++ TheoryConcepts/Ch5_SortingAlgorithm/Th2_SelectionSort.java
@@ -0,0 +1,36 @@
+package TheoryConcepts.Ch5_SortingAlgorithm;
+
+import java.util.Arrays;
+
+public class Th2_SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 2, -32, -7, 23, 10};
+        System.out.println("Before Sorting: "+ Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("After Sorting: "+Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr){
+        for(int i=0; i< arr.length; i++){
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swapped(arr, maxIndex, last);
+        }
+    }
+
+    static void swapped(int[] arr, int max, int last){
+        int temp = arr[max];
+        arr[max] = arr[last];
+        arr[last] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+        for(int i=0; i<end; i++){
+            if(arr[max]<arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+}
