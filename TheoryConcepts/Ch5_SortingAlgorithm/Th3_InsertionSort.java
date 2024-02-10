6c5afcc954542cd8a92b70dfe389980b0d0856cc
iamsayannayek <sayannayek1912@gmail.com>
2024-02-10 10:07:22 +0530

    Insertion Sort Added

---
+++ TheoryConcepts/Ch5_SortingAlgorithm/Th3_InsertionSort.java
@@ -0,0 +1,28 @@
+package TheoryConcepts.Ch5_SortingAlgorithm;
+
+import java.util.Arrays;
+
+public class Th3_InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 2, -32, -7, 23, 10};
+        System.out.println("Before Sorting: "+ Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println("After Sorting: "+Arrays.toString(arr));
+    }
+
+    static void insertionSort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j>0; j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
