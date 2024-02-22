50a73465751c44b95a6e0a438426dd1e8f26de87
iamsayannayek <sayannayek1912@gmail.com>
2024-02-22 09:47:59 +0530

    Added basic Recursion

---
+++ TheoryConcepts/Ch6_RecursionAndBacktracking/RecursionBasic.java
@@ -0,0 +1,17 @@
+package TheoryConcepts.Ch6_RecursionAndBacktracking;
+
+public class RecursionBasic {
+    public static void main(String[] args) {
+        //Print a number 1 to 5 without using Iteration & with one function
+        print(1);
+    }
+
+    static void print(int n){
+        if(n==5){ //This if function is called the Base Condition, where function didn't call itself recursively
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}
