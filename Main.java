ab66c192a98635804252aaa8c0df1020b10bcbae
iamsayannayek <sayannayek1912@gmail.com>
2024-02-26 20:30:44 +0530

    Added Array is Sorted

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sayan Nayek");
+        int[] arr = {2, 4, 5, 6, 8};
+        System.out.println(findSecondLargest(arr.length, arr));
+    }
+    public static int findSecondLargest(int n, int[] arr) {
+        // Write your code here.
+        Arrays.sort(arr);
+        for(int i=n-1; i>=0; i--){
+            if(arr[i] == arr[i - 1]){
+                continue;
+            }
+            return arr[i - 1];
+        }
+        return -1;
     }
 }
