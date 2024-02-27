70d8cc0b2233b4581dafc32725a7b7d02508ba0f
iamsayannayek <sayannayek1912@gmail.com>
2024-02-27 19:46:49 +0530

    Added Q3

---
+++ ParikhJainDSASheet/BasicDSA/ArraysQuestion/Q3_NonDecreasingArray.java
@@ -0,0 +1,23 @@
+package ParikhJainDSASheet.BasicDSA.ArraysQuestion;
+
+public class Q3_NonDecreasingArray {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isPossible(int[] nums, int n) {
+        // Write your code here.
+        int count = 0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]<nums[i-1]){
+                count++;
+                if(count>1) return false;
+                if(i==1 || nums[i-2]<=nums[i]){
+                    nums[i-1] = nums[i];
+                }else{
+                    nums[i] = nums[i-1];
+                }
+            }
+        }
+        return true;
+    }
+}
